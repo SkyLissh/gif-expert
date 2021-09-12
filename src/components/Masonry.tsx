df50a61a3441558d94971da42423222233b6032b
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2021-09-12 11:09:37 -0500

    :lipstick: Masonry Layout
    Remove temporal masonry and implements with Desandro's library

--- src/components/Masonry.tsx
+++ src/components/Masonry.tsx
@@ -1,5 +1,7 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import "./Masonry.css";
+import MasonryLayout from "masonry-layout";
+import imagesLoaded from "imagesloaded";
 
 import { API_KEY } from "../environment";
 import useFetch from "../hooks/useFetch";
@@ -12,6 +14,28 @@ export default function Masonry(): ReactElement {
 
 	const { data: gifs, error } = useFetch<GifResponse>(url.toString());
 
+	useEffect(() => {
+		if (gifs) {
+			const masonry = new MasonryLayout(".masonry", {
+				itemSelector: ".masonry__item",
+				columnWidth: ".masonry--resizer",
+				percentPosition: true,
+				gutter: 16
+			});
+
+			const imgLoad = imagesLoaded(".masonry");
+
+			imgLoad.on("progress", (instance, image) => {
+				console.log(image?.isLoaded);
+				image!.img.parentElement!.className = image!.isLoaded
+					? "masonry__item"
+					: "masonry__item masonry__item--broken";
+				// masonry.reloadItems?.();
+				masonry.layout!();
+			});
+		}
+	}, [gifs]);
+
 	if (error) {
 		return <div>Error: {error.message}</div>;
 	}
@@ -20,14 +44,20 @@ export default function Masonry(): ReactElement {
 	}
 
 	return (
-		<main className="masonry">
+		<main className="masonry__container">
 			<h2 className="masonry__title">
 				<span className="masonry__icon material-icons">trending_up</span>Trending
 			</h2>
-			<div className="masonry__container">
+			<div className="masonry">
+				<div className="masonry--resizer"></div>
 				{gifs.data.map((gif) => (
-					<div className="masonry__item" key={gif.id}>
-						<img className="masonry__img" src={gif.images.original.url} alt={gif.title} />
+					<div className="masonry__item masonry__item--loading" key={gif.id}>
+						<img
+							className="masonry__img"
+							src={gif.images.downsized.url}
+							alt={gif.title}
+							loading="lazy"
+						/>
 					</div>
 				))}
 			</div>
