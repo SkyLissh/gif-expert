3e9043827433a476d82a2a852a316293299db50d
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2021-09-13 11:17:59 -0500

    :art: MasonryLayout Component
    Make a reusable component for masonry layouts

--- src/components/Masonry.tsx
+++ src/components/Masonry.tsx
@@ -1,40 +1,25 @@
-import React, { ReactElement, useEffect } from "react";
+import React, { Dispatch, ReactElement, useState } from "react";
 import "./Masonry.css";
-import MasonryLayout from "masonry-layout";
-import imagesLoaded from "imagesloaded";
 
 import { API_KEY } from "../environment";
 import useFetch from "../hooks/useFetch";
 import GifResponse from "../models/GifResponse";
+import MasonryLayout from "./MasonryLayout";
 
-export default function Masonry(): ReactElement {
-	const url = new URL("https://api.giphy.com/v1/gifs/trending");
-	url.searchParams.append("api_key", API_KEY);
-	url.searchParams.append("limit", "20");
+type Props = {
+	itemSelector: string;
+	columnWidth: number | string;
+	gutter: number;
+	percentPosition: boolean;
+};
+
+export default function Masonry(props: Props): ReactElement {
+	const initUrl = new URL("https://api.giphy.com/v1/gifs/trending");
+	initUrl.searchParams.append("api_key", API_KEY);
+	initUrl.searchParams.append("limit", "13");
+	const [url, setUrl] = useState<string>(initUrl.toString());
 
 	const { data: gifs, error } = useFetch<GifResponse>(url.toString());
-
-	useEffect(() => {
-		if (gifs) {
-			const masonry = new MasonryLayout(".masonry", {
-				itemSelector: ".masonry__item",
-				columnWidth: ".masonry--resizer",
-				percentPosition: true,
-				gutter: 16
-			});
-
-			const imgLoad = imagesLoaded(".masonry");
-
-			imgLoad.on("progress", (instance, image) => {
-				console.log(image?.isLoaded);
-				image!.img.parentElement!.className = image!.isLoaded
-					? "masonry__item"
-					: "masonry__item masonry__item--broken";
-				// masonry.reloadItems?.();
-				masonry.layout!();
-			});
-		}
-	}, [gifs]);
 
 	if (error) {
 		return <div>Error: {error.message}</div>;
@@ -44,23 +29,44 @@ export default function Masonry(): ReactElement {
 	}
 
 	return (
-		<main className="masonry__container">
-			<h2 className="masonry__title">
-				<span className="masonry__icon material-icons">trending_up</span>Trending
-			</h2>
-			<div className="masonry">
-				<div className="masonry--resizer"></div>
-				{gifs.data.map((gif) => (
-					<div className="masonry__item masonry__item--loading" key={gif.id}>
-						<img
-							className="masonry__img"
-							src={gif.images.downsized.url}
-							alt={gif.title}
-							loading="lazy"
-						/>
-					</div>
-				))}
-			</div>
-		</main>
+		console.log(gifs),
+		(
+			<main className="masonry__container">
+				<h2 className="masonry__title">
+					<span className="masonry__icon material-icons">trending_up</span>Trending
+				</h2>
+				<button onClick={() => changeUrl(url, setUrl)}>Change url</button>
+				<MasonryLayout
+					className="masonry"
+					itemSelector="masonry__item"
+					columnWidth="masonry--resizer"
+					gutter={16}
+					percentPosition={true}
+				>
+					{gifs.data.map((gif) => (
+						<li
+							className={`masonry__item masonry__item--loading masonry__item--loading-${Math.round(
+								Math.random() * 3 + 1
+							)}`}
+							key={gif.id}
+						>
+							<img
+								className="masonry__img"
+								src={gif.images.original.webp}
+								alt={gif.title}
+								loading="lazy"
+							/>
+						</li>
+					))}
+				</MasonryLayout>
+			</main>
+		)
 	);
+}
+
+function changeUrl(url: string, setUrl: Dispatch<React.SetStateAction<string>>): void {
+	const initUrl = new URL(url);
+	initUrl.searchParams.append("offset", 20 + "");
+	setUrl(initUrl.toString());
+	console.log(url);
 }
---
+++ src/components/MasonryLayout.tsx
@@ -0,0 +1,42 @@
+import React, { ReactElement, useEffect, useRef } from "react";
+import Masonry from "masonry-layout";
+import imagesLoaded from "imagesloaded";
+
+interface Props {
+	children: React.ReactNode;
+	className: string;
+	itemSelector: string;
+	columnWidth: number | string;
+	gutter?: number;
+	percentPosition?: boolean;
+	onProgress?: ImagesLoaded.ImagesLoadedListener;
+}
+
+export default function MasonryLayout(props: Props): ReactElement {
+	const refMasonry = useRef<HTMLUListElement>(null);
+
+	useEffect(() => {
+		if (refMasonry.current) {
+			const masonry = new Masonry(refMasonry.current, {
+				itemSelector: `.${props.itemSelector}`,
+				columnWidth: `.${props.columnWidth}`,
+				gutter: props.gutter,
+				percentPosition: props.percentPosition
+			});
+
+			imagesLoaded(refMasonry.current).on("progress", (instance, image) => {
+				image!.img.parentElement!.className = image!.isLoaded
+					? props.itemSelector
+					: `${props.itemSelector} ${props.itemSelector}--broken`;
+				masonry.layout!();
+			});
+		}
+	});
+
+	return (
+		<ul className={props.className} ref={refMasonry}>
+			{typeof props.columnWidth === "string" && <div className={props.columnWidth}></div>}
+			{props.children}
+		</ul>
+	);
+}
